91d538f2a5c812daf06562fae815d8f3b11d69dc
jkkicks <jkkicks1@gmail.com>
2026-07-21 21:11:50 -0700

    fix: update "receive PO" icon

--- app/components/quotes/QuoteActionsDropdown.tsx
+++ app/components/quotes/QuoteActionsDropdown.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import { FileCheck } from "lucide-react";
 import { ToolpathIcon } from "~/components/icons/ToolpathIcon";
 
 interface QuoteActionsDropdownProps {
@@ -248,21 +249,7 @@ export default function QuoteActionsDropdown({
     ...(canReceivePo
       ? [
           {
-            icon: (
-              <svg
-                className="w-5 h-5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
-                />
-              </svg>
-            ),
+            icon: <FileCheck className="w-5 h-5" />,
             label: "Receive PO",
             onClick: () => {
               onReceivePo();
